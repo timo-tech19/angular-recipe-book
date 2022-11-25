6ee527054298e6712bbb8d1f77c89af0af691147
Timo Heman <timoheman16@gmail.com>
2022-11-25 12:23:25 +0100

    added dummy navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'angular-recipe-book';
+export class AppComponent implements OnInit {
+  activeRoute: string;
+
+  onNavigate(route: string) {
+    this.activeRoute = route;
+  }
+
+  ngOnInit() {
+    this.activeRoute = 'recipes';
+  }
 }
